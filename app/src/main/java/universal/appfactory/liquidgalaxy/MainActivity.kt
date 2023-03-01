d3a3d256db3984da48f9803bca330494f800972d
Datshin1511 <datshinr2002@gmail.com>
2023-03-02 01:56:40 +0530

    Layouts are changed

--- app/src/main/java/universal/appfactory/liquidgalaxy/MainActivity.kt
+++ app/src/main/java/universal/appfactory/liquidgalaxy/MainActivity.kt
@@ -1,6 +1,8 @@
 package universal.appfactory.liquidgalaxy
 
+import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -23,15 +25,18 @@ class MainActivity : AppCompatActivity() {
         val animator = layout.background as AnimationDrawable
 
         image.alpha = 0F
+        image.scaleX = 0.5F
+        image.scaleY = 0.5F
         buttonsView.alpha = 0F
 
+        image.animate().alpha(1F).scaleX(1F).scaleY(1F).setDuration(2000).start()
+        image.animate().translationY(-450F).setDuration(2000).start()
         val timer = object: CountDownTimer(5000, 1000) {
-            override fun onTick(p0: Long) {
-                image.animate().alpha(1F).setDuration(4000).translationY(-50F).start()
-            }
+            override fun onTick(p0: Long) { }
 
             override fun onFinish() {
-                buttonsView.animate().alpha(1F).setDuration(4000).start()
+                buttonsView.visibility = View.VISIBLE
+                buttonsView.animate().alpha(1F).setDuration(2000).start()
             }
         }
         timer.start()
@@ -39,6 +44,11 @@ class MainActivity : AppCompatActivity() {
         animator.setEnterFadeDuration(1000)
         animator.setExitFadeDuration(3000)
         animator.start()
+
+        image.setOnClickListener {
+            val intent = Intent("android.intent.action.VIEW", Uri.parse("https://www.liquidgalaxy.eu/"))
+            startActivity(intent)
+        }
 
     }
 
